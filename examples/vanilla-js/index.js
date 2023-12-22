79ada2ef0d2b0e7bc9020b979adf0af4daed1b33
Craig Ellenwood <73420772+inoculate23@users.noreply.github.com>
2023-12-22 01:14:36 -0800

    Update index.js

--- examples/vanilla-js/index.js
+++ examples/vanilla-js/index.js
@@ -9,9 +9,9 @@ const fileUpload = document.getElementById('file-upload');
 const imageContainer = document.getElementById('image-container');
 
 // Create a new object detection pipeline
-status.textContent = 'Loading model...';
-const detector = await pipeline('object-detection', 'Xenova/detr-resnet-50');
-status.textContent = 'Ready';
+let url = 'https://huggingface.co/datasets/Xenova/transformers.js-docs/resolve/main/butterfly.jpg';
+let upscaler = await pipeline('image-to-image', 'Xenova/swin2SR-classical-sr-x2-64');
+let output = await upscaler(url);
 
 fileUpload.addEventListener('change', function (e) {
     const file = e.target.files[0];
@@ -33,41 +33,3 @@ fileUpload.addEventListener('change', function (e) {
 });
 
 
-// Detect objects in the image
-async function detect(img) {
-    status.textContent = 'Analysing...';
-    const output = await detector(img.src, {
-        threshold: 0.5,
-        percentage: true,
-    });
-    status.textContent = '';
-    output.forEach(renderBox);
-}
-
-// Render a bounding box and label on the image
-function renderBox({ box, label }) {
-    const { xmax, xmin, ymax, ymin } = box;
-
-    // Generate a random color for the box
-    const color = '#' + Math.floor(Math.random() * 0xFFFFFF).toString(16).padStart(6, 0);
-
-    // Draw the box
-    const boxElement = document.createElement('div');
-    boxElement.className = 'bounding-box';
-    Object.assign(boxElement.style, {
-        borderColor: color,
-        left: 100 * xmin + '%',
-        top: 100 * ymin + '%',
-        width: 100 * (xmax - xmin) + '%',
-        height: 100 * (ymax - ymin) + '%',
-    })
-
-    // Draw label
-    const labelElement = document.createElement('span');
-    labelElement.textContent = label;
-    labelElement.className = 'bounding-box-label';
-    labelElement.style.backgroundColor = color;
-
-    boxElement.appendChild(labelElement);
-    imageContainer.appendChild(boxElement);
-}
